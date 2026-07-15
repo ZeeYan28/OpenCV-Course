00f7df6083b59e2dbe0832a980bdef53bca7aa1e
ZeeYan28 <xautayde@gmail.com>
2026-07-15 15:10:01 +0800

    Version 1

---
+++ read.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+
+img = cv.imread(('Resources/Photos/cat_large2.jpg'))
+
+cv.imshow('Cat', img)
+
+cv.waitKey(0)
+
+#Reading Videos
+# capture = cv.VideoCapture('Resources/Videos/dog.mp4')
+
+# while True:
+#     isTrue, frame = capture.read()
+
+#     cv.imshow('Video', frame)
+
+#     if cv.waitKey(20) & 0xff==ord('d'):
+#         break
+
+# capture.release()    
+# cv.destroyAllWindows()
