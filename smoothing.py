09f9d82d17f396dce89a10c9a3bb985feb1a4ef0
ZeeYan28 <xautayde@gmail.com>
2026-07-17 11:54:14 +0800

    1.0.2

--- smoothing.py
+++ smoothing.py
@@ -3,6 +3,20 @@ import cv2 as cv
 img = cv.imread('Resources/Photos/cats.jpg')
 cv.imshow('Cats', img)
 
+#Averaging
+average = cv.blur(img, (7,7))
+cv.imshow('Average Blur', average)
 
+#Gaussian Blur
+gauss = cv.GaussianBlur(img, (7,7), 0)
+cv.imshow('Gaussian Blur', gauss)
+
+#Median Blur
+median = cv.medianBlur(img, 7)
+cv.imshow('Median Blur', median)
+
+#Bilateral Blur - most effective but slowest
+bilateral = cv.bilateralFilter(img, 10, 15, 15)
+cv.imshow('Bilateral Blur', bilateral)
 
 cv.waitKey(0)
