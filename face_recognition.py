4f390dbb7b76444ac4121d652ec1030ffd3c5cd8
ZeeYan28 <xautayde@gmail.com>
2026-07-19 11:13:21 +0800

    Face Recognition Final

--- face_recognition.py
+++ face_recognition.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-haar_cascade = cv.CascadeClassifier('haarcascade_face.xml')
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
 
 people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
 # features = np.load('features.npy', allow_pickle=True)
@@ -10,13 +10,13 @@ people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kalin
 face_recognizer = cv.face.LBPHFaceRecognizer_create()
 face_recognizer.read('face_trained.yml')
 
-img = cv.imread(r'\Resources\Faces\val\ben_afflek\1.jpg')
+img = cv.imread(r'Resources\Faces\val\elton_john\1.jpg')
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Person', gray)
 
 #Detect the face in the image
-faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)\
+faces_rect = haar_cascade.detectMultiScale(gray, 1.1, 4)
 
 for (x, y, w, h) in faces_rect:
     faces_roi = gray[y:y+h, x:x+w]
